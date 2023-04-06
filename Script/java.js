e6d30f95756fc8cfe63680173826bed34177f82e
Maheen_Ali_dev <125376391+MaheenAli-dev@users.noreply.github.com>
2023-04-06 00:47:02 +0000

    Add files via upload

---
+++ Script/java.js
@@ -0,0 +1,108 @@
+// TASK 1
+
+// 5/APRIL/2023
+
+// JAVA SCRIPT FIRST CLASS
+
+// PRACTICE OF NINE CHAPTERS
+
+
+
+// alert("Welcome To Coding World")
+
+// var user = prompt('Put Your address', 'address')
+// alert("Successfully added")
+
+// var arr= [ 'hello' , 'hi']
+// document.write(arr)
+// console.log(arr)
+
+// var BanoQabil = "webDevelopement"
+// document.write(BanoQabil)
+//  console.log (BanoQabil)
+
+//  var BanoQabil = "WebDevelopement"
+// document.write(BanoQabil)
+//  console.log (BanoQabil)
+
+//  var Bano$Qabil = "web$Developer"
+// document.write(Bano$Qabil)
+//  console.log (Bano$Qabil)
+
+//  var Bano_Qabil = "web_Developer"
+// document.write(Bano_Qabil)
+//  console.log (Bano_Qabil)
+
+// var num1 =+ prompt("Num 1 Please")
+// var num2 =+ prompt("Num 2 Please")
+// document.write(popularNumber)
+// console.log(num1+num2)
+
+// var popularNumber = 10 - 30;
+// document.write(popularNumber)
+// console.log(10-30)
+
+// var num = 10;
+// var popularNumber = num + 100;
+// document.write(popularNumber)
+// console.log(num + 100)
+
+// var popularNumber = 3 * 12;
+// document.write(popularNumber)
+// console.log(3 * 12)
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// document.write(popularNumber)
+// console.log(num + anotherNum)
+
+// var num = 1;
+// var newNum = num++
+// document.write(newNum)
+// console.log(num ++)
+
+// var num = 1;
+// var newNum = ++num
+// document.write(newNum)
+// console.log(++num)
+
+// var num = 1;
+// var newNum = num--
+// document.write(newNum)
+// console.log(num--)
+
+// var num = 1;
+// var newNum = --num
+// document.write(newNum)
+// console.log(--num)
+
+// var totalCost = 1 + 3 * 4;
+// var totalCost = 1 + (3 * 4);
+// document.write(totalCost)
+// console.log(1 + (3 * 4))
+
+// var totalCost = (1 + 3) * 4;
+// document.write(totalCost)
+// console.log((1 + 3) * 4)
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// document.write(resultOfComputation)
+// console.log(((2 * 4) * 4) + 2)
+
+// resultOfComputation = (2 * 4) * (4 + 2)
+// document.write(resultOfComputation)
+// console.log((2 * 4) * (4 + 2))
+
+// var message = "Thanks, ";
+// var userName = "Mahi";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+var numberOfCats = prompt("How many pets?");
+var tooManyCats = numberOfpets + 1;
